8d4097cdafdc3ddb9f7f11cbd5b3e9b7950ea440
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2021-05-28 15:54:01 +0200

    Failing version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LdJsonTest';
+  waarde = {
+    "@context": "https://json-ld.org/contexts/person.jsonld",
+    "@id": "http://dbpedia.org/resource/John_Lennon",
+    name: "John Lennon",
+    born: "1940-10-09",
+    spouse: "http://dbpedia.org/resource/Cynthia_Lennon"
+  };
 }
